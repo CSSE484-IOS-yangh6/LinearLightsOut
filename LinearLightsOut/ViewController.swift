98cff7c4d3e90b66575f6f85ce7bb44115a5f3ea
rhit-yangh6 <yangh6@rose-hulman.edu>
2020-12-18 01:36:36 +0800

    Initial Commit

---
+++ LinearLightsOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinearLightsOut
+//
+//  Created by Hanyu Yang on 2020/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
